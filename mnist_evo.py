129ad447cd5cdcef6261a181c03f5b6ad06dbd3e
Zhichao Lu <mikelzc1990@gmail.com>
2019-04-17 17:32:28 -0400

    beta1

--- mnist_evo.py
+++ mnist_evo.py
@@ -21,14 +21,13 @@ import utils
 parser = argparse.ArgumentParser(description='PyTorch Evolutionary Deep CNN weights training')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--init_channels', type=int, default=64, help='num of init channels')
-parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--pop_size', type=int, default=20, help='population size')
-parser.add_argument('--gens', type=int, default=1000, help='maximum number of generations')
-parser.add_argument('--tour_size', type=int, default=10, help='tournament size')
+parser.add_argument('--tour_size', type=int, default=5, help='tournament size')
 parser.add_argument('--p_mut', type=float, default=0.1, help='probability for mutation')
 parser.add_argument('--eta_m', type=float, default=30.0, help='polynomial mutation parameter')
 parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+parser.add_argument('--epochs', type=int, default=350, help='num of training epochs')
 
 args = parser.parse_args()
 
@@ -39,13 +38,16 @@ utils.create_exp_dir(args.save)
 device = 'cuda'
 seed = args.seed
 population_size = args.pop_size
-generations = args.gens
 tournament_size = args.tour_size
 p_mut = args.p_mut
 eta_m = args.eta_m
 batch_size = args.batch_size
 # calculate number of batches for each individual
 n_batch = int(50000 / population_size / batch_size)
+# calculate report frequency
+report_freq = population_size*3
+generations = population_size*3*args.epochs
+
 assert n_batch > 0
 
 log_format = '%(asctime)s %(message)s'
@@ -95,6 +97,16 @@ def random_weights(dim, mu=0., sigma=1.):
     return np.random.normal(mu, sigma, dim)
 
 
+def random_combination(iterable, sample_size):
+    """Random selection from itertools.combinations(iterable, r)."""
+    pool = tuple(iterable)
+    n = len(pool)
+
+    indices = sorted(random.sample(range(n), sample_size))
+
+    return tuple(pool[i] for i in indices)
+
+
 def load_parameters(model, params_to_load):
     lb = 0
     for name, param in model.named_parameters():
@@ -107,14 +119,65 @@ def load_parameters(model, params_to_load):
     return
 
 
+def polynomial_mutation(X, prob_mut=0.1, eta_mut=30):
+    # dynamic bound since the actual bounds for weights are unknown
+    # let's assume each mutation cannot mutate the weight beyond +/-20% from current value
+    lb = np.array([0.8 * x if x > 0 else 1.2 * x for x in X])
+    ub = np.array([1.2 * x if x > 0 else 0.8 * x for x in X])
+
+    Y = np.full(X.shape, np.inf)
+
+    do_mutation = np.random.rand(X.shape[0]) < prob_mut
+
+    Y[:] = X
+
+    xl = lb[do_mutation]
+    xu = ub[do_mutation]
+
+    X = X[do_mutation]
+
+    delta1 = (X - xl) / (xu - xl)
+    delta2 = (xu - X) / (xu - xl)
+
+    mut_pow = 1.0 / (eta_mut + 1.0)
+
+    rand = np.random.rand(X.shape[0])
+    mask = rand <= 0.5
+    mask_not = np.logical_not(mask)
+
+    deltaq = np.zeros(X.shape)
+
+    xy = 1.0 - delta1
+    val = 2.0 * rand + (1.0 - 2.0 * rand) * (np.power(xy, (eta_mut + 1.0)))
+    d = np.power(val, mut_pow) - 1.0
+    deltaq[mask] = d[mask]
+
+    xy = 1.0 - delta2
+    val = 2.0 * (1.0 - rand) + 2.0 * (rand - 0.5) * (np.power(xy, (eta_mut + 1.0)))
+    d = 1.0 - (np.power(val, mut_pow))
+    deltaq[mask_not] = d[mask_not]
+
+    # mutated values
+    _Y = X + deltaq * (xu - xl)
+
+    # back in bounds if necessary (floating point issues)
+    _Y[_Y < xl] = xl[_Y < xl]
+    _Y[_Y > xu] = xu[_Y > xu]
+
+    # set the values for output
+    Y[do_mutation] = _Y
+
+    return Y
+
+
 def evaluate(pop, train_queue, criterion):
     # every individual in population will only be evaluated on one mini-batch
     # assuming mini-batch size = total_num_training_data / population size
     pop_eval, indv_counter, batch_counter = [], 0, 0
     with torch.no_grad():
         for step, (inputs, targets) in enumerate(train_queue):
-            print('indv id: {}'.format(indv_counter))
-            print('batch id: {}'.format(batch_counter))
+            # print('indv id: {}'.format(indv_counter))
+            # print('batch id: {}'.format(batch_counter))
             inputs, targets = inputs.to(device), targets.to(device)
             # when we move on to next individual in population
             if batch_counter == 0:
@@ -141,6 +204,33 @@ def evaluate(pop, train_queue, criterion):
                 break
 
     return pop_eval
+
+
+def infer(elite, valid_queue, criterion):
+    net = Net().to(device).eval()
+    load_parameters(net, elite[1])
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for step, (inputs, targets) in enumerate(valid_queue):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs, _ = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            if step % 50 == 0:
+                logging.info('valid %03d %e %f', step, test_loss/total, 100.*correct/total)
+
+    acc = 100.*correct/total
+    logging.info('valid acc %f', 100. * correct / total)
+
+    return test_loss/total, acc
 
 
 def main():
@@ -193,12 +283,8 @@ def main():
 
     # evaluation
     population = evaluate(population, train_loader, criterion)
-    print(population)
-    1/0
-    elite_idx = np.argmin([x[0] for x in population])
-    print('train %03d %e %e %f' % (0, p_mut,
-                                   population[elite_idx][0],
-                                   population[elite_idx][-1]))
+    elite_idx = np.argmax([x[0] for x in population])  # find individual w/ highest accuracy
+    logging.info('train acc %04d %f', 0, population[elite_idx][0])
 
     # main loop of evolution
     for gen in range(1, generations + 1):
@@ -206,13 +292,9 @@ def main():
         # best from the sample becomes parent
         tmp = sorted(sample, key=lambda i: i[0])
 
-        winner, loser = tmp[0], tmp[-1]
+        winner, loser = tmp[-1], tmp[0]
 
-        # update mutation probability - linear drop
-        p_mut = p_mut - (p_mut - 1 / n_params_trainable) / generations
-        child = [(np.inf, PolynomialMutation(eta_mut=eta_m,
-                                             prob_mut=p_mut,
-                                             xl=-2, xu=2).do(winner[1]))]
+        child = [(0, polynomial_mutation(winner, prob_mut=p_mut, eta_mut=eta_m))]
         child = evaluate(child, train_loader, criterion)
 
         # replace loser in population with child
@@ -221,13 +303,11 @@ def main():
 
         population.pop(remove_idx)
         population += child
-        if gen % 100 == 0:
-            elite_idx = np.argmin([x[0] for x in population])
-            print('train %03d %e %e %f' % (0, p_mut,
-                                           population[elite_idx][0],
-                                           population[elite_idx][-1]))
+        if gen % report_freq == 0:
+            elite_idx = np.argmax([x[0] for x in population])
+            logging.info('train acc %04d %f', 0, population[elite_idx][0])
+            infer(population[elite_idx], test_loader, criterion)
 
 
 if __name__ == '__main__':
     main()
-
